1f475984149cac5e71615e502ce1349aef4be127
Nahuelchamorro <47437099+Nahuelchamorro@users.noreply.github.com>
2019-02-07 21:57:04 -0300

    1St commit
    class 1 utn fra

---
+++ jsEntradaSalida-3.js
@@ -0,0 +1,11 @@
+/*Debemos lograr tomar un dato por 'ID'
+y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+	var person;
+	person=elNombre.value;
+
+    alert(person);
+}
+
+
---
+++ jsEntradaSalida-4.js
@@ -0,0 +1,17 @@
+/* 
+	Debemos lograr tomar un dato por 'PROMPT' 
+	y lo muestro por 'getElementById' al presionar el botón 'mostrar'
+*/
+function mostrar()
+{
+   person=prompt("coloque su nombre:", );
+ alert("este nombre será notado debajo.");
+   elNombre.value=person;
+   document.getElementById('elNombre').value=person;
+
+
+
+
+
+}
+
