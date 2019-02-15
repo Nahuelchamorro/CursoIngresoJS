13ef7c6f12b4efcd556104efdb82f0f486f7b316
Nahuelchamorro <47437099+Nahuelchamorro@users.noreply.github.com>
2019-02-15 17:56:12 -0300

    If 1 al 4
    faltan del 4 al 10

---
+++ jsInstruccionIF (2).js
@@ -0,0 +1,16 @@
+function mostrar()
+{
+//tomo la edad 
+var edad;
+  
+  edad= parseInt(document.getElementById('edad').value);
+
+  if (edad > 17){
+   
+
+  	alert("es mayor de edad");
+    } 
+
+
+
+}//FIN DE LA FUNCIÓN
---
+++ jsInstruccionIF (3).js
@@ -0,0 +1,21 @@
+function mostrar()
+{
+//tomo la edad  
+var edad;
+  
+  edad= parseInt(document.getElementById('edad').value);
+
+  if (edad > 17){
+   
+
+  	alert("es mayor de edad");
+  	 }
+   else
+   	{ 
+   	alert("es menor de edad")
+
+    } 
+
+
+
+}//FIN DE LA FUNCIÓN
